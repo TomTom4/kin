a4386fe886f9d9e65f43ecb468d8fa8e7c61ad5d
TomPouv <contact@thomas-pouvreau.fr>
2025-11-09 14:43:58 +0100

    test: basic endpoint to get all appointments

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,19 +1,86 @@
+import datetime
+import uuid
+
+import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from app.main import app
+from app.main import AppointmentController, app, get_appointment_service
+from app.models import Appointment
 
-client = TestClient(app)
+
+@pytest.fixture
+def controlled_service():
+    return AppointmentController()
 
 
-def test_hello_world():
-    response = client.get("/")
-    assert response.status_code == 200
+@pytest.fixture
+def basic_client():
+    client = TestClient(app)
+    return client
+
+
+@pytest.fixture
+def override_app(controlled_service: AppointmentController):
+    app.dependency_overrides[get_appointment_service] = lambda: controlled_service
+    try:
+        yield app
+    finally:
+        app.dependency_overrides = {}
+
+
+@pytest.fixture
+def with_controlled_service_client(override_app: FastAPI):
+    client = TestClient(override_app)
+    return client
+
+
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    return client
 
 
 class TestAppointments:
 
-    def test_get_all_appointments_with_no_appointments(self):
+    def test_get_all_appointments_with_no_appointments(self, client: TestClient):
         response = client.get("/appointments")
         assert response.status_code == 200
         assert response.json() == {"appointments": []}
 
+    def test_get_all_appointments_with_one_appointment(
+        self,
+        with_controlled_service_client: TestClient,
+        controlled_service: AppointmentController,
+    ):
+        appointment: Appointment = Appointment(
+            id=uuid.uuid4(),
+            title="test",
+            start_at=datetime.datetime.now() + datetime.timedelta(days=1),
+            patient_id=uuid.uuid4(),
+            therapist_id=uuid.uuid4(),
+        )
+        controlled_service.appointments.append(appointment)
+        response = with_controlled_service_client.get("/appointments")
+        assert response.status_code == 200
+        appointments = response.json()["appointments"]
+        assert len(appointments) == 1
+
+    def test_get_all_appointments_with_many_appointment(
+        self,
+        with_controlled_service_client: TestClient,
+        controlled_service: AppointmentController,
+    ):
+        appointment: Appointment = Appointment(
+            id=uuid.uuid4(),
+            title="test",
+            start_at=datetime.datetime.now() + datetime.timedelta(days=1),
+            patient_id=uuid.uuid4(),
+            therapist_id=uuid.uuid4(),
+        )
+        controlled_service.appointments.append(appointment)
+        controlled_service.appointments.append(appointment)
+        response = with_controlled_service_client.get("/appointments")
+        assert response.status_code == 200
+        appointments = response.json()["appointments"]
+        assert len(appointments) > 1
